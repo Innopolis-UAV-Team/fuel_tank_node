531e4abfec152578379ccbacae819e0bf84e0dc0
AsiiaPine <asiiapine@gmail.com>
2024-01-18 09:42:14 +0300

    add hal_i2c

--- Src/dronecan_application/modules/fuel_tank.hpp
+++ Src/dronecan_application/modules/fuel_tank.hpp
@@ -18,17 +18,18 @@ class VtolFuelTank {
 public:
     VtolFuelTank();
     int8_t init(uint8_t tank_id, uint16_t is_reserved);
-    void process();
+    int8_t process();
     int8_t update_data();
 
 private:
-    As5600Periphery as5600{};
     uint32_t _last_publish_time_ms{0};
     uint32_t _last_update_time_ms{0};
-    FuelTankStatus_t _tank_info{};
     uint8_t _transfer_id = 0;
     uint8_t _log_transfer_id = 0;
-    // int8_t publish_log();
+
+    As5600Periphery as5600{};
+    FuelTankStatus_t _tank_info{};
+
     DebugLogMessage_t init_mes{};    
     DebugLogMessage_t proc_mes{};    
 };
